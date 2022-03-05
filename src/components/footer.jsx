6da922199bcde90de67fc5a44ebd89d25e2c198b
Abdelqwy <abdelqwysayed@gmail.com>
2022-03-05 08:39:31 -0800

    footer-update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import logo from '../assets/images/logo.png'
-import facebook from '../assets/images/Facebook.svg'
-import instagram from '../assets/images/Instagram.svg'
-import twitter from '../assets/images/Twitter.svg'
-import { Link } from 'react-router-dom';
+import React from "react";
+import logo from "../assets/images/logo.png";
+import facebook from "../assets/images/Facebook.svg";
+import instagram from "../assets/images/Instagram.svg";
+import twitter from "../assets/images/Twitter.svg";
 const Footer = () => {
   return (
-    <div className="footer"  data-aos="fade-right" data-aos-duration="2000">
+    <div className="footer" data-aos="fade-right" data-aos-duration="2000">
       <div className="section1">
-          <img src={logo} alt="logo" className="logo-footer" />
+        <img src={logo} alt="logo" className="logo-footer" />
         <div className="social">
-          <img src={facebook} alt="facebook" className="social-icons"/>
-          <img src={twitter} alt="twitter" className="social-icons"/>
-          <img src={instagram} alt="instagram"className="social-icons" />
+          <img src={facebook} alt="facebook" className="social-icons" />
+          <img src={twitter} alt="twitter" className="social-icons" />
+          <img src={instagram} alt="instagram" className="social-icons" />
         </div>
       </div>
       <div className="section2">
